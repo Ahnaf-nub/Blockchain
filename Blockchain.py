af328f95c793744fa819d52abc52f86e233a6b51
Mir Muhammad Abidul Haq (Ahnaf) <76505613+Ahnaf-nub@users.noreply.github.com>
2024-08-14 05:38:01 +0600

    Create Blockchain.py

---
+++ Blockchain.py
@@ -0,0 +1,59 @@
+import hashlib
+import json
+from time import time
+from uuid import uuid4
+from fastapi import FastAPI
+
+app = FastAPI()
+
+class Blockchain(object): #will store the transactions
+    def proof_of_work(self, last_proof):
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+        return proof
+    
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self, proof, previous_hash=None):
+        block = {
+            'index':len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+        # reset the current list of transactions
+        self.current_transactions = []
+        self.chain
+        return block
+    
+    def new_transaction(self, sender, recipient, amount):
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+        return self.last_block['index'] + 1
+    
+def new_block(self):
+    pass
+def new_transaction(self):
+    pass
+
+@staticmethod
+def hash(block):
+    block_string = json.dumps(block, sort_keys=True).encode()
+    return hashlib.sha256(block_string).hexdigest()
+
+def valid_proof(last_proof, proof):
+    guess = f'{last_proof}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:4] == "0000"
+
+@property
+def last_block(self):
+    return self.chain[-1]
