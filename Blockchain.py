aca6ef9dc506742077c84039acb54c899f1df42f
Mir Muhammad Abidul Haq (Ahnaf) <76505613+Ahnaf-nub@users.noreply.github.com>
2024-08-14 06:45:29 +0600

    Update Blockchain.py

--- Blockchain.py
+++ Blockchain.py
@@ -2,35 +2,34 @@ import hashlib
 import json
 from time import time
 from uuid import uuid4
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from urllib.parse import urlparse
+import requests 
 
 app = FastAPI()
 
-class Blockchain(object): #will store the transactions
-    def proof_of_work(self, last_proof):
-        proof = 0
-        while self.valid_proof(last_proof, proof) is False:
-            proof += 1
-        return proof
-    
+class Blockchain:
     def __init__(self):
         self.chain = []
         self.current_transactions = []
-        self.new_block(previous_hash=1, proof=100)
+        # Create the genesis block
+        self.new_block(previous_hash='1', proof=100)
+        self.nodes = set()
 
     def new_block(self, proof, previous_hash=None):
         block = {
-            'index':len(self.chain) + 1,
+            'index': len(self.chain) + 1,
             'timestamp': time(),
             'transactions': self.current_transactions,
             'proof': proof,
             'previous_hash': previous_hash or self.hash(self.chain[-1]),
         }
-        # reset the current list of transactions
+        # Reset the current list of transactions
         self.current_transactions = []
-        self.chain
+        self.chain.append(block)
         return block
-    
+
     def new_transaction(self, sender, recipient, amount):
         self.current_transactions.append({
             'sender': sender,
@@ -39,21 +38,150 @@ class Blockchain(object): #will store the transactions
         })
         return self.last_block['index'] + 1
     
-def new_block(self):
-    pass
-def new_transaction(self):
-    pass
+    def register_node(self, address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
 
-@staticmethod
-def hash(block):
-    block_string = json.dumps(block, sort_keys=True).encode()
-    return hashlib.sha256(block_string).hexdigest()
+    def valid_chain(self, chain):
+        last_block = chain[0]
+        current_index = 1
 
-def valid_proof(last_proof, proof):
-    guess = f'{last_proof}{proof}'.encode()
-    guess_hash = hashlib.sha256(guess).hexdigest()
-    return guess_hash[:4] == "0000"
+        while current_index<len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print("\n-----------\n")
 
-@property
-def last_block(self):
-    return self.chain[-1]
+            if block["previous_hash"] != self.hash(last_block):
+                return False
+            if not self.valid_proof(last_block['proof'] ,block['proof']):
+                return False
+            last_block = block
+            current_index += 1
+        return True
+    
+    def resolve_conflicts(self):
+        neighbours = self.nodes
+        new_chain = None
+        max_length = len(self.chain)
+
+        for node in neighbours:
+            response = requests.get(f'https://{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                if length>max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+
+    @staticmethod
+    def hash(block):
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+    def proof_of_work(self, last_proof):
+        proof = 0
+        while not self.valid_proof(last_proof, proof):
+            proof += 1
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
+# Instantiate the Blockchain
+blockchain = Blockchain()
+# Generate a globally unique address for this node
+node_identifier = str(uuid4()).replace('-', '')
+
+class Transaction(BaseModel):
+    sender: str
+    recipient: str
+    amount: int
+
+@app.get('/mine')
+async def mine():
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    # Reward for mining a new block
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1,
+    )
+
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return response
+
+@app.post('/transactions/new')
+async def new_transaction(transaction: Transaction):
+    index = blockchain.new_transaction(transaction.sender, transaction.recipient, transaction.amount)
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return response
+
+@app.get('/chain')
+async def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return response
+
+@app.post('/nodes/register')
+async def register_nodes(request: Request):
+    values = await request.json()
+    nodes = values.get('nodes')
+    if nodes is None:
+        return "Error: Please supply a valid list of nodes", 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        'message': 'New nodes have been added',
+        'total_nodes': list(blockchain.nodes),
+    }
+    return response, 201
+
+@app.get('/nodes/resolve')
+async def consensus(request: Request):
+    replaced = blockchain.resolve_conflicts()
+    if replaced:
+        response = {
+            'message': 'Our chain was replaced',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Our chain is authoritative',
+            'chain': blockchain.chain
+        }
+    return response
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host='127.0.0.1', port=5000)
